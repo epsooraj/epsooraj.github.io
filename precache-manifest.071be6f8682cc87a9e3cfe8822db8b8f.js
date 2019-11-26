2bd36c4f48e577216c621bb32fbb203dddda714f
epsooraj <epsooraj4@gmail.com>
2019-11-27 02:38:53 +0530

    Build with homepage .

---
+++ precache-manifest.071be6f8682cc87a9e3cfe8822db8b8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae27bd878e9de90ef108fadff85039e6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e8f60b0ae89fba9ba85c",
+    "url": "./static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3940621c6b452e0ed3f6",
+    "url": "./static/js/2.ec3136e8.chunk.js"
+  },
+  {
+    "revision": "e8f60b0ae89fba9ba85c",
+    "url": "./static/js/main.15405892.chunk.js"
+  },
+  {
+    "revision": "7f0222fc59018060a1d9",
+    "url": "./static/js/runtime-main.91ef73f0.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
