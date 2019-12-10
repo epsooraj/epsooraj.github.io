9cf62a47f6c45e044885c4a7aa28324d0b2f0e4e
epsooraj <epsooraj4@gmail.com>
2019-12-11 00:24:42 +0530

    Patches

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store";
-import home from "./views/home";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import Home from './views/home';
 
 function App() {
-	return (
-		<Provider store={store}>
-			<Router>
-				{/* Add nav which will be in every page */}
-				<Switch>
-					<Route exact path="/">
-						{home}
-					</Route>
-				</Switch>
-			</Router>
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <Router>
+        {/* Add nav which will be in every page */}
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </Router>
+    </Provider>
+  );
 }
 
 export default App;
--- src/views/home/index.js
+++ src/views/home/index.js
@@ -1,12 +1,19 @@
-import React from "react";
-import "./home.css";
+import React, { Component } from 'react';
+import './home.css';
+import Navbar from '../../components/navbar';
+import NameTitle from '../../components/nametitle';
+import { MDBBtn } from 'mdbreact';
 
-function home() {
-	return (
-		<div>
-			<h1>Ep Sooraj</h1>
-		</div>
-	);
+class Home extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+        <NameTitle />
+        {/* <MDBBtn gradient='peach'>Test</MDBBtn> */}
+      </React.Fragment>
+    );
+  }
 }
 
-export default home;
+export default Home;
