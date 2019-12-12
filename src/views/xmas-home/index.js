4d72dd5cad1d457cc0e74580d15a15af61cdc868
epsooraj <epsooraj4@gmail.com>
2019-12-12 13:05:26 +0530

    Adding rotating xmas tree'

--- src/views/xmas-home/index.js
+++ src/views/xmas-home/index.js
@@ -3,15 +3,16 @@ import './home.css';
 import Navbar from '../../components/navbar';
 import NameTitle from '../../components/nametitle';
 import SeriesSet from '../../components/series-set';
+import RotatingXmasTree from '../../components/xmas-tree';
 import { MDBBtn } from 'mdbreact';
 
 class Home extends Component {
   render() {
     return (
       <React.Fragment>
-        {/* <Navbar /> */}
         <SeriesSet />
         <NameTitle />
+        <RotatingXmasTree />
       </React.Fragment>
     );
   }
