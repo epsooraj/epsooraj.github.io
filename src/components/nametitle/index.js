6151d7a112036b1680976e8391f20db4e3eda172
epsooraj <epsooraj4@gmail.com>
2019-12-12 14:32:16 +0530

    Updating text

--- src/components/nametitle/index.js
+++ src/components/nametitle/index.js
@@ -9,7 +9,7 @@ export default class NameTitle extends Component {
         <MDBCol>
           <h2 className='display-3 text-center brand glitch'>Ep Sooraj</h2>
           <p className='lead text-center desc'>
-            Machine Learning Enthusiast | Backend Web Developer
+            Machine Learning Enthusiast<br></br>Backend Web Developer
           </p>
         </MDBCol>
       </MDBRow>
