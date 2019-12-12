95bccc87602c028f1652119cc24e5fd19661bbba
epsooraj <epsooraj4@gmail.com>
2019-12-12 13:24:54 +0530

    Rearranging css imports

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
+import App from './App';
+
 import './index.css';
 import './media-query.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
