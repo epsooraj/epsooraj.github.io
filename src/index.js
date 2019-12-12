6eaa7764d1d0cbe95947a8f581d9aceb2a58c999
epsooraj <epsooraj4@gmail.com>
2019-12-12 14:33:38 +0530

    Adding xmas fonts

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
+import './assets/fonts/xmas-fonts.css'
+
 import App from './App';
 
 import './index.css';
