f3c3673fd0e198231231dcbfb585dcd168ce990b
epsooraj <epsooraj4@gmail.com>
2019-12-12 14:11:35 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a034edbe334075e1a08de7eaa8d6b539.js"
+  "/precache-manifest.5446d65392b050c4ec3a122445d10604.js"
 );
 
 self.addEventListener('message', (event) => {
