41e6c5359f94601429c425f2c58c7b3226f63e34
epsooraj <epsooraj4@gmail.com>
2019-12-13 18:19:44 +0530

    Contact only

--- src/views/xmas-home/index.js
+++ src/views/xmas-home/index.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import './home.css';
+
 import NameTitle from '../../components/nametitle';
 import SeriesSet from '../../components/series-set';
+
+import Skills from '../../components/sections/skills';
 import Experience from '../../components/sections/experience';
+import Education from '../../components/sections/education';
 import Contact from '../../components/sections/contact';
+
 import { MDBBtn } from 'mdbreact';
 
 class Home extends Component {
@@ -12,7 +17,6 @@ class Home extends Component {
       <React.Fragment>
         <SeriesSet />
         <NameTitle />
-        <Experience />
         <Contact />
       </React.Fragment>
     );
