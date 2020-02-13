179f09776ab055636ad2bc5457190f1c63274f05
E P Sooraj <epsooraj4@gmail.com>
2020-02-13 15:06:20 +0530

    Rearranging

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -2,27 +2,13 @@ from django.contrib import admin
 from .models import Experience, Education, Skill, Language, Portfolio, Language, Technology, PortfolioType
 
 # Register your models here.
-
-class PortfolioAdmin(admin.ModelAdmin):
-		list_display = ('title', 'portfolio_type')
-		ordering = ('title',)
-		search_fields = ('title', 'language', 'technology')
-
-admin.site.register(Portfolio, PortfolioAdmin)
-
 class ExperienceAdmin(admin.ModelAdmin):
+	fields = ('title', 'company', 'location', ('start_date', 'end_date'), 'description')
 	list_display = ('title', 'company', 'start_date', 'end_date')
 	ordering = ('start_date',)
 	search_fields = ('title', 'company', 'location')
 
 admin.site.register(Experience, ExperienceAdmin)
-
-class EducationAdmin(admin.ModelAdmin):
-	list_display = ('degree', 'college', 'start_date', 'end_date')
-	ordering = ('start_date',)
-	search_fields = ('degree', 'college')
-
-admin.site.register(Education, EducationAdmin)
 
 class SkillAdmin(admin.ModelAdmin):
 	list_display = ('skill', 'rating', 'skill_type')
@@ -30,6 +16,21 @@ class SkillAdmin(admin.ModelAdmin):
 	search_fields = ('skill',)
 
 admin.site.register(Skill, SkillAdmin)
+
+class EducationAdmin(admin.ModelAdmin):
+	fields = ('degree', 'college', ('start_date', 'end_date'))
+	list_display = ('degree', 'college', 'start_date', 'end_date')
+	ordering = ('start_date',)
+	search_fields = ('degree', 'college')
+
+admin.site.register(Education, EducationAdmin)
+
+class PortfolioAdmin(admin.ModelAdmin):
+		list_display = ('title', 'portfolio_type')
+		ordering = ('title',)
+		search_fields = ('title', 'language', 'technology')
+
+admin.site.register(Portfolio, PortfolioAdmin)
 
 admin.site.register(Language)
 admin.site.register(Technology)
