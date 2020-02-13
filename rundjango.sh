7e035ac38040a3f895a67702c14c744c998772d4
E P Sooraj <epsooraj4@gmail.com>
2020-02-13 15:00:51 +0530

    Run commandline arguments

--- rundjango.sh
+++ rundjango.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-DATABASE_URL=$(heroku config:get DATABASE_URL -a ep-portfolio-api) python3 manage.py runserver
+if [ $# -gt 0 ];
+then
+	env_var="DATABASE_URL=$(heroku config:get DATABASE_URL -a ep-portfolio-api) python3 manage.py "
+else
+	echo "No arguments supplied"
+	exit
+fi
+
+command="$env_var $1"
+eval $command
