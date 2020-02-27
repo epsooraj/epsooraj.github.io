041075972bd39898daed7bcd810749e12a567548
Ep Sooraj <epsooraj4@gmail.com>
2020-02-27 20:33:12 +0530

    Rearranging

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './App.css';
 
 import './assets/plugins/bootstrap/bootstrap.min.css';
-// import './assets/plugins/owl-carousel/owl.carousel.min.css';
 import './assets/plugins/owl-carousel/owl.theme.default.min.css';
 import './assets/plugins/magnific-popup/magnific-popup.min.css';
-// import './assets/css/app.min.css';
 import './assets/plugins/font-awesome/css/all.css';
 import './assets/plugins/themify/themify-icons.min.css';
+// import './assets/css/app.min.css';
+// import './assets/plugins/owl-carousel/owl.carousel.min.css';
 
 function App() {
   return (
