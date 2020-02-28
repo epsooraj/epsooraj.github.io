11088869d4c4ea62842ed128de9c3bd0e440eef1
Ep Sooraj <epsooraj4@gmail.com>
2020-02-28 23:24:55 +0530

    Added header, images, bootstrap

--- src/views/sections/home/header/index.js
+++ src/views/sections/home/header/index.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
+
 import './header.css';
+import AgencyClassicBg from '../../../../assets/images/agency-classic-bg.jpg';
+
+import {Navbar} from 'react-bootstrap';
 
 class Header extends Component {
 	render() {
+		var header_style = {
+			backgroundImage: `url(${AgencyClassicBg})`
+		};
+
+		var navbar_style = {
+			background: 'transparent',
+		};
+
+		// var 
+
 		return (
-			<div>
-				Header
-			</div>
+			<header className="header" style={header_style}>
+				<Navbar className="noshadow" style={navbar_style}>
+					<Navbar.Brand className="navbrand text-white">Ep Sooraj</Navbar.Brand>
+  					<Navbar.Toggle />
+  					<Navbar.Collapse className="justify-content-end">
+    					<Navbar.Text className="text-white">
+      						Hamburger
+    					</Navbar.Text>
+  					</Navbar.Collapse>
+				</Navbar>
+
+				<div>
+					<h1 className="main-desc text-white">
+						Let me bring your<br />
+						Imaginations to Life.
+					</h1>
+				</div>
+			</header>
 		);
 	}
+
 }
 
 export default Header;
