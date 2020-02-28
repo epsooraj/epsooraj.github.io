ee3d1107d7f90e916440c758b11cbd96d5c297ca
Ep Sooraj <epsooraj4@gmail.com>
2020-02-28 11:51:00 +0530

    Moving mediaquery to css

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import './media-query.css';
+import './assets/css/media-query.css';
 
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
