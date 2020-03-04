f7e569fc3236422f567e1020a8b08398d8db26d7
Ep Sooraj <epsooraj4@gmail.com>
2020-03-05 00:23:57 +0530

    Adding translations, changing timing

--- js/main.js
+++ js/main.js
@@ -3,8 +3,11 @@
 // ——————————————————————————————————————————————————
 
 const phrases = [
-  'If you don\'t know why you are here, <br>Then, probably, you shouldn\'t be here.',
-  'Test 2'
+  'If you don\'t know why you are here,<br>Then, probably, you shouldn\'t be here.',
+  'Wenn Sie nicht wissen,<br>warum Sie hier sind, sollten Sie wahrscheinlich nicht hier sein.',
+  'Si vous ne savez pas pourquoi vous êtes ici,<br>alors, vous ne devriez probablement pas être ici.',
+  'Si no sabes por qué estás aquí,<br>entonces, probablemente, no deberías estar aquí.',
+  '如果您不知道为什么在这里，<br>那么，也许您不应该在这里。'
 ]
 
 const el = document.querySelector('.quote-text')
@@ -13,7 +16,7 @@ const fx = new TextScramble(el)
 let counter = 0
 const next = () => {
   fx.setText(phrases[counter]).then(() => {
-    setTimeout(next, 6000)
+    setTimeout(next, 8000)
   })
   counter = (counter + 1) % phrases.length
 }
